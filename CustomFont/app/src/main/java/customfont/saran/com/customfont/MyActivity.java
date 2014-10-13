8d92358844cf7d461211ac2d8155d78bc1b5530e
Saran <saranshajibabu@gmail.com>
2014-10-13 23:06:10 +0800

    Custom Font Example #71

---
+++ CustomFont/app/src/main/java/customfont/saran/com/customfont/MyActivity.java
@@ -0,0 +1,23 @@
+package customfont.saran.com.customfont;
+
+import android.app.Activity;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.widget.TextView;
+
+
+public class MyActivity extends Activity {
+
+    TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+
+        textView = (TextView) findViewById(R.id.textview);
+        //Assets folder should be directly in main folder and not inside res
+        Typeface customFont = Typeface.createFromAsset(getAssets(),"fonts/Roboto-BoldItalic.ttf");
+        textView.setTypeface(customFont);
+    }
+
+}
