37bd4889d49c978e02b7319a2195256c589f1e7e
Saran <saranshajibabu@gmail.com>
2015-03-03 22:46:25 +0800

    Added alpha animation and animation listener

--- SimpleAnimation/app/src/main/java/android/saran/com/simpleanimation/MainActivity.java
+++ SimpleAnimation/app/src/main/java/android/saran/com/simpleanimation/MainActivity.java
@@ -4,18 +4,47 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
 
 
-public class MainActivity extends ActionBarActivity {
-
+public class MainActivity extends ActionBarActivity implements View.OnClickListener{
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(this);
     }
 
     public void onClick(View v){
-        startActivity(new Intent(this,SecondActivity.class));
-        overridePendingTransition(R.anim.slide_in_left,R.anim.slide_out_left);
+        Animation animation = AnimationUtils.loadAnimation(this,R.anim.fade_out);
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                button.setVisibility(View.INVISIBLE);
+                startActivity(new Intent(MainActivity.this, SecondActivity.class));
+                overridePendingTransition(R.anim.slide_in_left,R.anim.slide_out_left);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        button.startAnimation(animation);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        button.setVisibility(View.VISIBLE);
     }
 }
--- SimpleAnimation/app/src/main/java/android/saran/com/simpleanimation/SecondActivity.java
+++ SimpleAnimation/app/src/main/java/android/saran/com/simpleanimation/SecondActivity.java
@@ -3,14 +3,17 @@ package android.saran.com.simpleanimation;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
+import android.widget.Button;
 
 
-public class SecondActivity extends ActionBarActivity {
-
+public class SecondActivity extends ActionBarActivity implements View.OnClickListener{
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(this);
     }
 
     public void onClick(View v){
