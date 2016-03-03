5d3a04fd905151f19956a76eaf17203678b2c7b9
saran <saranbs2013@gmail.com>
2016-03-03 14:18:01 +0800

    Test Library

---
+++ SampleApp/samplelibrary/src/main/java/com/sample/samplelibrary/TestClass.java
@@ -0,0 +1,14 @@
+package com.sample.samplelibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * Created by Saran on 3/3/16.
+ */
+public class TestClass {
+
+    public static void test(Context context){
+        Toast.makeText(context,"Testing....",Toast.LENGTH_SHORT).show();
+    }
+}
